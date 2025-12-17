e0b3e3a1b2f3dfc5f48afac1b44fffb0a2e6e1de
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2025-12-17 22:04:33 +0100

    Singleton

---
+++ DesignPatterns/Singleton/Singleton/Player.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Player
+    {
+        #region Field
+        private static Player instance;
+
+        #endregion
+
+        #region Properties
+        public static Player Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new Player();
+                }
+
+                return instance;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        private Player()
+        {
+        }
+
+        #endregion
+
+        #region Method
+        /// <summary>
+        /// A Hello message printet to the concole
+        /// </summary>
+        public void SayHi()
+        {
+            Console.WriteLine("Hi");
+        }
+
+
+        #endregion
+    }
+}
---
+++ DesignPatterns/Singleton/Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using Singleton;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Singleton");
+
+        //Getting the Player instance to call on the method SayHi()
+        Player.Instance.SayHi();
+    }
+}
