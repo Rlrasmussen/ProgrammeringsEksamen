e2ee2a31bc0b17d07c338255b6378cb022eb9b66
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2025-12-17 21:41:47 +0100

    Mapper

---
+++ DesignPatterns/Command/Program.cs
@@ -0,0 +1,56 @@
+﻿using Command;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Command pattern");
+
+        Dimmer dimser = new Dimmer();
+
+        dimser.AddCommands();
+
+        bool running = true;
+
+        while (running)
+        {
+            string message = Console.ReadLine().ToLower();
+
+            if (message == null || message=="")
+            {
+                running = false;
+            }
+
+            dimser.RunDutter(message);
+
+        }
+    }
+
+
+    class Dimmer
+    {
+        #region Fields
+        private Dictionary<string, ICommand> commands = new Dictionary<string, ICommand>();
+
+
+        #endregion
+        internal void RunDutter(string message)
+        {
+            if (commands.ContainsKey(message))
+            {
+                commands[message].Execute();
+            }
+            else
+            {
+                Console.WriteLine("You missed");
+            }
+        }
+
+        internal void AddCommands()
+        {
+            commands.Add("s", new ShootCommand());
+            commands.Add("d", new DogdeCommand());
+        }
+    }
+}
---
+++ DesignPatterns/Command/ShootCommand.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Command
+{
+    public class ShootCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Phew phew");
+        }
+    }
+
+    public class DogdeCommand : ICommand
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Dogde");
+        }
+    }
+}
