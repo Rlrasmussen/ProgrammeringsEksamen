a4b1be9957fef9322c6e737fd7d31ef687768c55
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2025-12-20 16:02:57 +0100

    PrototypePattern

---
+++ DesignPatterns/Prototype/PrototypePattern/GameObject.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrototypePattern
+{
+    public class GameObject
+    {
+        public int health;
+        public int speed;
+        private ConsoleColor color = ConsoleColor.White;
+
+        public ConsoleColor Color { get => color; set => color = value; }
+
+        public GameObject ShallowCopy()
+        {
+            return (GameObject)this.MemberwiseClone();
+        }
+
+        public GameObject DeepCopy()
+        {
+            //Copying the value types
+            GameObject coby = (GameObject)this.MemberwiseClone();
+
+            //Creating an new instance
+            coby = new GameObject();
+
+            return coby;
+        }
+    }
+}
---
+++ DesignPatterns/Prototype/PrototypePattern/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace PrototypePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GameObject go = new GameObject();
+
+            Console.WriteLine($"go color is {go.Color}");
+
+            //Shallow copy
+            GameObject goShallow = go.ShallowCopy();
+            //Console.WriteLine($"goShallow color is {goShallow.Color}");
+
+            //Changing color Shallow Copy//
+            /*Console.WriteLine("goShallow has chenged color to Red");
+            goShallow.Color = ConsoleColor.Red;
+            Console.WriteLine($"go color is {go.Color}");
+            Console.WriteLine($"goShallow color is {goShallow.Color}");
+
+            Console.WriteLine("go has chenged color to Red");
+            go.Color = ConsoleColor.Red;
+            Console.WriteLine($"go color is {go.Color}");
+            Console.WriteLine($"goShallow color is {goShallow.Color}"); */
+
+            //Deep Copy
+            GameObject goDeep = go.DeepCopy();
+            Console.WriteLine($"goShallow color is {goDeep.Color} /n");
+
+
+            //Changing color Deep Copy//
+            //Console.WriteLine("goDeep has chenged color to Red");
+            //goDeep.Color = ConsoleColor.Red;
+            //Console.WriteLine($"go color is {go.Color}");
+            //Console.WriteLine($"goDeep color is {goDeep.Color}");
+
+            Console.WriteLine("go has chenged color to Red");
+            go.Color = ConsoleColor.Red;
+            Console.WriteLine($"go color is {go.Color}");
+            Console.WriteLine($"goDeep color is {goDeep.Color}");
+        }
+    }
+}
