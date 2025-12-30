2b5dab27b8c7583f888548b96d8357fdbc2e4654
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2025-12-30 21:49:12 +0100

    Graph

---
+++ Algorithm/Graph/Graph/Edge.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph
+{
+    public class Edge<T>
+    {
+        #region Field
+        private Node<T> from;
+        private Node<T> to;
+        private float weight;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructor
+        public Edge(Node<T> from, Node<T> to, float weigth = 1)
+        {
+            this.from = from;
+            this.to = to;
+            this.weight = weigth;
+        }
+
+        #endregion
+    }
+}
---
+++ Algorithm/Graph/Graph/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Graph
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph<string> myGraph = new Graph<string>();
+
+            myGraph.AddNode("Kage");
+            myGraph.AddNode("Mand");
+
+            myGraph.AddEdge("Kage", "Dame");
+
+        }
+    }
+}
