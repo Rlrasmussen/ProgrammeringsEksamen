b65deaccbd0cc53e4a91a7943c2deca96db4c54b
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2025-12-31 14:36:22 +0100

    DepthFirst

---
+++ Algorithm/DFS/DFS/GraphClass.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DFS
+{
+    public class GraphClass<T>
+    {
+        #region field
+        private List<Node<T>> nodes = new List<Node<T>>();
+
+
+        #endregion
+
+        #region properties
+        internal List<Node<T>> Nodes { get => nodes; private set => nodes = value; }
+
+        #endregion
+
+        #region constructor
+
+
+        #endregion
+
+        #region method
+        public void AddNode(T value)
+        {
+            Nodes.Add(new Node<T>(value));
+        }
+
+        /// <summary>
+        /// Can only go one way
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        public void AddDirectedEdge(T from, T to, float weight = 1)
+        {
+            Node<T> fromNode = Nodes.Find(x => EqualityComparer<T>.Default.Equals(x.Data, from));
+            Node<T> toNode = Nodes.Find(x => EqualityComparer<T>.Default.Equals(x.Data, to));
+
+            //Adding a edge between (start) fromNode to (end) toNode
+            //if (!fromNode.Equals(default(T)) && !toNode.Equals(default(T)))
+            if (fromNode != null && toNode != null)
+            {
+                fromNode.AddEdge(toNode, weight);
+            }
+            else
+            {
+                Console.WriteLine("Node doesn't exist");
+            }
+        }
+
+        /// <summary>
+        /// Can go both ways
+        /// </summary>
+        /// <param name="from"></param>
+        /// <param name="to"></param>
+        public void AddEdge(T from, T to, float weight = 1)
+        {
+            Node<T> fromNode = Nodes.Find(x => x.Data.Equals(from));
+            Node<T> toNode = Nodes.Find(x => x.Data.Equals(to));
+
+            //Adding a edge between(start) fromNode to(end) toNode
+            if (!fromNode.Equals(default(T)) && !toNode.Equals(default(T)))
+            {
+                fromNode.AddEdge(toNode, weight); //fromNode going to toNode
+                toNode.AddEdge(fromNode, weight); //toNode going to fromNode
+
+            }
+            else
+            {
+                Console.WriteLine("Node doesn't exist");
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ Algorithm/DFS/DFS/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace DFS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphClass<string> myGraph = new GraphClass<string>();
+
+            #region Adding nodes to myGraph
+            myGraph.AddNode(Park.Entrance.ToString());
+            myGraph.AddNode(Park.SlotMachines.ToString());
+            myGraph.AddNode(Park.IcaBlaster.ToString());
+            myGraph.AddNode(Park.FunHoude.ToString());
+            myGraph.AddNode(Park.Hotdogs.ToString());
+            myGraph.AddNode(Park.LogFlume.ToString());
+            myGraph.AddNode(Park.BigDipper.ToString());
+            myGraph.AddNode(Park.Rollercoaster.ToString());
+            myGraph.AddNode(Park.RocketShips.ToString());
+            myGraph.AddNode(Park.Cinema3D.ToString());
+            myGraph.AddNode(Park.PirateShip.ToString());
+            myGraph.AddNode(Park.GhostTrain.ToString());
+            myGraph.AddNode(Park.Carousel.ToString());
+            myGraph.AddNode(Park.FlyingChairs.ToString());
+
+            #endregion
+
+            #region Adding edges to myGraph
+            myGraph.AddEdge(Park.Entrance.ToString(), Park.IcaBlaster.ToString());
+            myGraph.AddEdge(Park.Entrance.ToString(), Park.FunHoude.ToString());
+            myGraph.AddEdge(Park.Entrance.ToString(), Park.SlotMachines.ToString());
+
+            myGraph.AddEdge(Park.IcaBlaster.ToString(), Park.SlotMachines.ToString());
+            myGraph.AddEdge(Park.IcaBlaster.ToString(), Park.RocketShips.ToString());
+            myGraph.AddEdge(Park.IcaBlaster.ToString(), Park.Cinema3D.ToString());
+            myGraph.AddEdge(Park.IcaBlaster.ToString(), Park.FunHoude.ToString());
+
+            myGraph.AddEdge(Park.Cinema3D.ToString(), Park.RocketShips.ToString());
+            myGraph.AddEdge(Park.Cinema3D.ToString(), Park.FunHoude.ToString());
+            myGraph.AddEdge(Park.Cinema3D.ToString(), Park.PirateShip.ToString());
+
+            myGraph.AddEdge(Park.SlotMachines.ToString(), Park.Hotdogs.ToString());
+            myGraph.AddEdge(Park.SlotMachines.ToString(), Park.RocketShips.ToString());
+
+            myGraph.AddEdge(Park.GhostTrain.ToString(), Park.RocketShips.ToString());
+            myGraph.AddEdge(Park.GhostTrain.ToString(), Park.BigDipper.ToString());
+            myGraph.AddEdge(Park.GhostTrain.ToString(), Park.Carousel.ToString());
+            myGraph.AddEdge(Park.GhostTrain.ToString(), Park.FlyingChairs.ToString());
+
+            myGraph.AddEdge(Park.BigDipper.ToString(), Park.Rollercoaster.ToString());
+            myGraph.AddEdge(Park.BigDipper.ToString(), Park.LogFlume.ToString());
+
+            myGraph.AddEdge(Park.Hotdogs.ToString(), Park.LogFlume.ToString());
+            myGraph.AddEdge(Park.Carousel.ToString(), Park.FlyingChairs.ToString());
+            #endregion
+
+            DepthFirst<string> DFS = new DepthFirst<string>(myGraph);
+
+            Node<string> start = myGraph.Nodes.Find(x => x.Data == Park.Entrance.ToString());
+            Node<string> goal = myGraph.Nodes.Find(x => x.Data == Park.GhostTrain.ToString());
+
+            DFS.Search(start, goal);
+
+            List<Node<string>> pathDFS = DFS.TrackPath(start, goal);
+
+            foreach (var node in pathDFS)
+            {
+                Console.WriteLine(node.Data);
+            }
+        }
+
+        public enum Park
+        {
+            Entrance,
+            SlotMachines,
+            IcaBlaster,
+            FunHoude,
+            Hotdogs,
+            LogFlume,
+            BigDipper,
+            Rollercoaster,
+            RocketShips,
+            Cinema3D,
+            PirateShip,
+            GhostTrain,
+            Carousel,
+            FlyingChairs
+        }
+    }
+
+
+}
