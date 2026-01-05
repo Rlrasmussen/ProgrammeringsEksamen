d319738622b17f185e34b45f3fbdc8a2c4da6c6a
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2026-01-05 19:19:44 +0100

    UDP

---
+++ Network/UDP/Network03-1/SimpleUDP/UDPServer.cs
@@ -0,0 +1,19 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+UdpClient udpServer = new UdpClient(1234);
+Console.WriteLine("Serveren er klar til at modtage data...");
+while (true)
+{
+    IPEndPoint clientEndPoint = new IPEndPoint(IPAddress.Any, 0);
+    byte[] receivedData = udpServer.Receive(ref clientEndPoint);
+
+    string receivedMessage = Encoding.ASCII.GetString(receivedData);
+
+    Console.WriteLine($"Modtaget: {receivedMessage} fra {clientEndPoint}");
+
+    string responseMessage = "Tak for beskeden!";
+    byte[] responseData = Encoding.ASCII.GetBytes(responseMessage);
+    udpServer.Send(responseData, clientEndPoint);
+}
---
+++ Network/UDP/Network03-1/UDPCLient/UDPClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+UdpClient udpClient = new UdpClient();
+IPAddress serverIP = IPAddress.Parse("127.0.0.1");
+int serverPort = 1234;
+IPEndPoint EndPoint = new IPEndPoint(serverIP, serverPort);
+
+while (true)
+{
+    Console.WriteLine("Skriv en besked til serveren:");
+    string message = Console.ReadLine();
+    byte[] sendData = Encoding.ASCII.GetBytes(message);
+    udpClient.Send(sendData, EndPoint);
+
+    //Answer from server! 
+    byte[] receivedData = udpClient.Receive(ref EndPoint);
+    string receivedMessage = Encoding.ASCII.GetString(receivedData);
+    
+    Console.WriteLine($"Svar fra serveren: {receivedMessage} på adresse: {EndPoint}");
+
+}
