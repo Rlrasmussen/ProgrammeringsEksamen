b9d6e1b097f5f26d357dcb52a53850641f1e7d53
Rikke Lund Rasmussen <soede_rikke@hotmail.com>
2026-01-05 15:01:23 +0100

    TCP

---
+++ Network/TCP/Network02-1/Network02TCP/Server.cs
@@ -0,0 +1,50 @@
+﻿using System.Net.Sockets;
+using System.Net;
+
+TcpListener server = new TcpListener(IPAddress.Any, 12000);
+server.Start();
+Console.WriteLine("Server started... listening on port 12000");
+
+while (true)
+{
+    TcpClient client = server.AcceptTcpClient();
+    Thread clientThread = new Thread(() => HandleClient(client));
+    clientThread.Start();
+}
+
+void HandleClient(TcpClient client)
+{
+    Guid clientId = Guid.NewGuid();
+    Console.WriteLine($"Client {clientId} connected.");
+    // Send client name to the client
+    StreamWriter writer = new StreamWriter(client.GetStream());
+    writer.Flush();
+    // Receive and send messages
+    StreamReader reader = new StreamReader(client.GetStream());
+    try
+    {
+        
+        while (client.Connected)
+        {
+            string message = reader.ReadLine();
+            if (message != null)
+            {
+                Console.WriteLine($"Received message from {clientId}: {message}");
+                //Echoing message
+                writer.WriteLine(message + " From server");
+                writer.Flush();
+
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception occurred for client {clientId}: {ex.Message}");
+    }
+    finally
+    {
+        Console.WriteLine($"Client disconnected: {clientId}");
+
+        client.Dispose();
+    }
+}
---
+++ Network/TCP/Network02-1/TcpClient/Client.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Sockets;
+
+TcpClient client = new TcpClient();
+client.Connect("localhost", 12000);
+Console.WriteLine("Connected to server...");
+// Start a thread to receive messages
+Thread receiveThread = new Thread(() => ReceiveMessages(client));
+receiveThread.Start();
+
+// Send messages
+StreamWriter writer = new StreamWriter(client.GetStream());
+while (true)
+{
+    string message = Console.ReadLine();
+    writer.WriteLine(message);
+    writer.Flush();
+}
+
+void ReceiveMessages(TcpClient client)
+{
+    StreamReader reader = new StreamReader(client.GetStream());
+    while (client.Connected)
+    {
+        string message = reader.ReadLine();
+        if (message != null)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
